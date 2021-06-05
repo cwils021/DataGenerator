7cf372ca9ec171452342b72a9162496ab410d646
Christopher Wilson <chrisgwilson678@gmail.com>
2021-06-05 18:53:54 -0400

    update data_generator.py

--- src/data_generator.py
+++ src/data_generator.py
@@ -25,3 +25,16 @@ Bookings
     - price
     - Total Cost
 '''
+import numpy as np
+import pandas as pd
+
+from faker import Faker
+from datetime import date
+from collections import OrderedDict
+
+
+# Generate User Data
+
+# Generate Room Data
+
+# Generate Bookings Data
